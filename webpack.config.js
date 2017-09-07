12cd95bb55272af8d9d037b45f739b834a9998f6
Mike Semko <msemko@gmail.com>
2017-09-07 05:11:46 -0400

    Configure html-webpack-plugin, add index.ejs template to assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 "use strict";
 
-const path = require("path");
+const path              = require("path"),
+      HtmlWebpackPlugin = require("html-webpack-plugin"),
+      htmlWebpackPlugin = new HtmlWebpackPlugin({
+        title: "Webpack Boilerplate Attempt 4",
+        template: "./assets/index.ejs"
+      });
 
 module.exports = {
   entry: "./assets/js/main.js",
   output: {
     path: path.resolve(__dirname, "public"),
     filename: "[hash:10].bundle.js"
-  }
+  },
+  plugins: [
+    htmlWebpackPlugin
+  ]
 };
