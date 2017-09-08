70c871aee428386f51a9868791440f3ef05fed94
Mike Semko <msemko@gmail.com>
2017-09-07 22:46:41 -0400

    Configure webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const path              = require("path"),
+      webpack           = require("webpack"),
+      hmrPlugin         = new webpack.HotModuleReplacementPlugin(),
       HtmlWebpackPlugin = require("html-webpack-plugin"),
       htmlWebpackPlugin = new HtmlWebpackPlugin({
         title: "Webpack Boilerplate Attempt 4",
@@ -14,6 +16,22 @@ module.exports = {
     filename: "[hash:10].bundle.js"
   },
   plugins: [
-    htmlWebpackPlugin
-  ]
+    htmlWebpackPlugin,
+    hmrPlugin
+  ],
+  devServer: {
+    open: true,
+    hot: true,
+    port: 4000,
+    historyApiFallback: true,
+    overlay: {
+      warnings: true,
+      errors: true
+    },
+    proxy: {
+      "/api/": "http://localhost:3000"
+    },
+    noInfo: true,
+    contentBase: path.resolve(__dirname, "assets")
+  }
 };
